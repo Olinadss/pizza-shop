99ecf169657ed151eaf6345e8acfd48896e8d101
Danilo dos Santos <danilo.souza@brisabr.com.br>
2024-08-14 15:59:36 -0300

    feat: faz mocks dos pedidos e dos detalhes do pedido

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -8,6 +8,7 @@ import { getManagedRestaurantMock } from './get-managed-restaurant-mock'
 import { getMonthCanceledOrdersAmountMock } from './get-month-canceled-orders-amount-mock'
 import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
 import { getMonthRevenueMock } from './get-month-revenue'
+import { getOrdersMock } from './get-orders-mock'
 import { getPopularProductsMock } from './get-popular-products-mock'
 import { getProfileMock } from './get-profile-mock'
 import { registerRestaurantMock } from './register-restaurant-mock'
@@ -26,6 +27,7 @@ export const worker = setupWorker(
   getProfileMock,
   updateProfileMock,
   getManagedRestaurantMock,
+  getOrdersMock,
 )
 
 export async function enableMSW() {
